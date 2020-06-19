ced883f74c9ae9fd502464811e48adbdfb78d5e3
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-06-19 11:58:26 -0400

    Add snippet on constructors

---
+++ methods/constructors/PizzaPi.java
@@ -0,0 +1,11 @@
+public class PizzaPi {
+	public final float pi = 3.14f;
+	private final float circumference;
+	private final String cheese = "cheddar";	
+
+	public PizzaPi(int radius) {
+	circumference = 2 * pi * radius; 
+	}
+
+}
+
