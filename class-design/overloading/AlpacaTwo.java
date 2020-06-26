d1ed5a7138b2f25967ce4922389331e74d880d4a
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-06-26 16:53:40 -0400

    add snippets on overloading.

---
+++ class-design/overloading/AlpacaTwo.java
@@ -0,0 +1,8 @@
+public class AlpacaTwo {
+	protected double getHeight() throws NotEnoughDataException {
+	return 13;
+	}
+	protected int getFavoriteNumber() {
+		return 42;
+	}
+}
---
+++ class-design/overloading/SuriAlpaca.java
@@ -0,0 +1,9 @@
+public class SuriAlpaca extends AlpacaTwo {
+	protected double getHeight() throws Exception {
+	 return 7;
+	}
+
+	protected int getFavoriteNumber() throws NotEnoughDataException {
+	}
+
+}
