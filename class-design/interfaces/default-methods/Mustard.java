12037df792dfea98c2aece95a19fda9a96cfe83c
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-06-30 13:59:12 -0400

    Add snippets on default methods

---
+++ class-design/interfaces/default-methods/Mustard.java
@@ -0,0 +1,8 @@
+public interface Mustard {
+	public default void spread();
+	public int youMustard() {
+	System.out.println("You mustard forgot to put a method body on line 2.");
+ 	System.out.println("You also mustard forgot to use the `default` keyword for this method.");
+return 1337;
+	}	
+}
